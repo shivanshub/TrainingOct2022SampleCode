5cff82f0a70136837809a5e40c4a3bffafec4ff8
Shivanshu Baweja <Shivanshub@gmail.com>
2022-10-19 06:54:27 +0530

    Added code for Extensions Method , Tuple , Lambda

---
+++ Feature/Extensions.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Feature
+{
+    internal static class ProjectExtensions
+    {
+
+        public static string ClientSpecificDateFormat(this DateTime time)
+        {
+            return $"Exact time is {time.ToLongTimeString()}";
+        }
+
+
+        public static int[] ConvertToArray(this IList<int> list)
+        {
+            var intArray = new int[list.Count];
+
+            var i = 0;
+            foreach (var number in list)
+            {
+                intArray[i] = number;
+                i++;
+
+            }
+
+            return intArray;
+        }
+    }
+}
---
+++ Feature/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Collections.Generic;
+
+namespace Feature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tuples example 
+
+            //Without names 
+            (double, int) t1 = (4.5, 3);
+            Console.WriteLine($"Tuple with elements {t1.Item1} and {t1.Item2}.");
+
+            //With Names 
+            (double Sum, int Count) t2 = (4.5, 3);
+            Console.WriteLine($"Sum of {t2.Count} elements is {t2.Sum}.");
+
+
+
+            //Returns from functions
+
+            var t3 = new TupleExample().ReturnsTwoNumbers();
+            Console.WriteLine($"Sum of {t3.Item1} elements is {t3.Item2}.");
+
+
+
+
+
+
+            //Extension method 
+
+            Console.WriteLine( DateTime.Now.ClientSpecificDateFormat());
+
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var abc = list.ConvertToArray();
+
+
+
+
+
+
+
+
+            // Lambda expression
+
+
+
+            var res = list.Find(delegate (int n)
+            {
+                if (n == 5) return true;
+                else return false;
+            });
+
+
+            var res2 = list.Find(x => x == 5);
+
+            var res3 = list.Find(x =>
+            {
+                return x >5 ? true:false;
+            });
+        }
+    }
+}
